a5757f3dfa654c9bf647736da5e96b2a7ad2428a
Viktor Tishchenko <9043258451vvt@gmail.com>
2023-06-25 15:30:43 +0600

    feat: nothing

--- ch5/src/electron/main.ts
+++ ch5/src/electron/main.ts
@@ -18,6 +18,18 @@ function createWindow() {
 
 app.whenReady().then(() => {
   createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
 ipcMain.handle('getContent', () => {
